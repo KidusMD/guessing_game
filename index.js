46c21c1a0ce468b9a19fd7c67931172cae1023d5
kidusmehretu <kidusmehretu@gmail.com>
2022-09-16 21:28:35 +0300

    numbers added

--- index.js
+++ index.js
@@ -1,21 +1,86 @@
-//*********************************** */
-//How/what to do something...
-//(something) take this and do something with it
+let button0 = document.getElementById('0')
+let button1 = document.getElementById('1')
+let button2 = document.getElementById('2')
+let button3 = document.getElementById('3')
+let button4 = document.getElementById('4')
+let button5 = document.getElementById('5')
+let button6 = document.getElementById('6')
+let button7 = document.getElementById('7')
+let button8 = document.getElementById('8')
+let button9 = document.getElementById('9')
 
-//normal function
-
-//DO!
-//(something) = this is what you will take
-// countCharactersInString('')
-//******************************* */
-//make a game to guess random numbers from 1 to 10
 let turnBoolean = true
 let randomNumber = Math.random();
 let guessedNumbers = [];
+let multipliedNumber = randomNumber * 10;
+let finalNumber = Math.round(multipliedNumber)
+
+
+let buttons = document.getElementsByClassName("button")
+console.log(buttons)
+for (let button of buttons) {
+    console.log(button)
+    button.addEventListener('click', function (e) {
+        let player1 = document.getElementById('name1').value;
+
+        let player2 = document.getElementById('name2').value;
+
+        if (turnBoolean === true) {
+            document.getElementById('gamer').innerHTML = 'It is ' + player1 + "'s turn!"
+        } else {
+            document.getElementById('gamer').innerHTML = 'It is ' + player2 + "'s turn!"
+        }
+        turnBoolean = !turnBoolean
+
+        // let clickedButtons = document.getElementsByClassName("clicked")
+        //take the clicked number and store itlet
+        let clickedNumber = e.target.id
+        console.log(clickedNumber)
+        let clickedElement = document.getElementById(clickedNumber)
+        //compare it with to guess no
+        if (parseInt(clickedNumber) === finalNumber) {
+            console.log("we win")
+            document.getElementById('winner').style.display === ""
+            if (turnBoolean === true) {
+                document.getElementById('winner').innerHTML = player1.toUpperCase() + '  WINS!'
+            } else {
+                document.getElementById('winner').innerHTML = player2.toUpperCase() + ' WINS!'
+            }
+            clickedElement.classList.add("win")
+            document.querySelectorAll(".clicked").forEach(div => {
+                div.classList.remove("clicked");
+            })
+            // console.log(clickedButtons)
+            // for (let clicked of clickedButtons) {
+            //     clicked.classList.remove("clicked")
+            //     console.log(clicked)
+            // }
+
+            // for (var i = 0; i < clickedButtons.length; i++) {
+            //     clickedButtons[i].classList.remove("clicked")
+            //     console.log(clickedButtons[i])
+            // }
+
+            randomNumber = Math.random()
+            multipliedNumber = randomNumber * 10;
+            finalNumber = Math.round(multipliedNumber)
+            setTimeout(() => {
+                clickedElement.classList.remove("win")
+            }, 3000)
+        } else {
+            console.log("we lose")
+            clickedElement.classList.add("clicked")
+        }
+        //if same , we win 
+        //else we add "clicked" to the element class
+    });
+}
+
+//******************************* */
+//make a game to guess random numbers from 1 to 10
+
 
 function MakeRandomNum() {
-    let multipliedNumber = randomNumber * 10;
-    let finalNumber = Math.round(multipliedNumber)
 
     let player1 = document.getElementById('name1').value;
 
