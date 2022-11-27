c88bd10d71121859f1b2c0d9fbe4a1bbf4d53738
Home <kidusmehretu@gmail.com>
2022-11-27 20:39:35 +0300

    background changer removed from  guessing game

--- script.js
+++ script.js
@@ -125,32 +125,3 @@ function MakeRandomNum() {
     console.log('finalNumber', finalNumber)
 }
 
-
-const icons = document.querySelectorAll('.icon');
-const body = document.querySelector('body');
-
-console.log(buttons);
-
-icons.forEach(function (icon) {
-    icon.addEventListener('click', function (e) {
-        console.log(e.target);
-        if (e.target.id === 'grey') {
-            body.style.backgroundColor = e.target.id;
-        }
-        if (e.target.id === 'white') {
-            body.style.backgroundColor = e.target.id;
-        }
-        if (e.target.id === 'blue') {
-            body.style.backgroundColor = e.target.id;
-        }
-        if (e.target.id === 'yellow') {
-            body.style.backgroundColor = e.target.id;
-        }
-        if (e.target.id === 'red') {
-            body.style.backgroundColor = e.target.id;
-        }
-        if (e.target.id === 'brown') {
-            body.style.backgroundColor = e.target.id;
-        }
-    })
-})
